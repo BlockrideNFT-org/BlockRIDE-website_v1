be9e9dcb3f0b67eb5c0292d3618af9ee2f69cc3a
horlard <oladipuposaulih@gmail.com>
2023-04-01 14:46:09 +0100

    innit scaffold

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import tw, { styled } from "twin.macro";
+import "./App.css";
+
+function App() {
+  return <Container>App</Container>;
+}
+
+const Container = styled.div`
+  ${tw`mt-[300px]`}
+`;
+export default App;
