66dc3830839f574da89dbacabdb884f236bec8ec
horlard <oladipuposaulih@gmail.com>
2024-01-15 14:17:02 +0100

    update

--- src/app/views/Home/index.tsx
+++ src/app/views/Home/index.tsx
@@ -2,7 +2,7 @@ import Faqs from "./components/Faqs";
 import IntroSection from "./components/IntroSection";
 import InvestmentProcess from "./components/InvestmentProcess";
 import ManagementProcess from "./components/ManagementProcess";
-import Waitlist from "./components/WaitList";
+import Waitlist from "./components/Waitlist";
 
 export default function Home() {
   return (
